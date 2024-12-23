ff4c5bc38b956382b97aefbf25e80649cd74a8d6
EmanuelCAC <emanuelc279@gmail.com>
2024-12-23 11:13:42 -0300

    Skills section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [isActive, setIsActive] = useState<string>('Home')
+  const [isActive, setIsActive] = useState('Home')
+  const [scrollDown, setScrollDown] = useState(false)
+
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    if (window.scrollY > 0) {
+      setScrollDown(true)
+    }
+
+    if (window.scrollY <= 0) {
+      setScrollDown(false)
+    }
+
+    if (window.scrollY < 800) {
+      setIsActive('Home')
+    }
+
+    if (window.scrollY >= 800) {
+      setIsActive('Skills')
+    }
+  })
 
   return (
     <>
-      <div className="flex flex-row gap-8 bg-gray-100 py-4 px-16 static drop-shadow-md">
+      <div className={`flex flex-row gap-8 bg-gray-100 py-4 px-16 fixed top-0 w-full z-20 ${scrollDown && 'drop-shadow-md'}`}>
         <a
           href="#home"
           className={`text-black text-xl ${isActive == 'Home'? 'font-semibold' : 'font-normal'}`}
@@ -42,8 +62,10 @@ function App() {
         </a>
       </div>
 
+      
+
       <section id='home'>
-        <div className='flex flex-row items-center justify-center w-full py-32 px-60 bg-slate-50'>
+        <div className='flex flex-row items-center justify-center w-full px-60 bg-slate-100 h-[100dvh]'>
           <div className='flex flex-1 flex-col'>
             <h1 className='text-2xl font-semibold text-start text-black'>
               About me
@@ -64,8 +86,76 @@ function App() {
       </section>
 
       <section id='skills'>
-        <div>
-          
+        <div className='flex flex-col items-center w-full px-60 bg-slate-200 h-[93dvh] py-16'>
+          <h1>
+            Skills
+          </h1>
+
+          <table className='my-auto border-separate border-spacing-0 drop-shadow-xl'>
+            <tr>
+              <th className='rounded-tl-xl'>Backend</th>
+              <th>Frontend</th>
+              <th>Mobile</th>
+              <th>Database</th>
+              <th className='rounded-tr-xl'>Other</th>
+            </tr>
+            <tr>
+              <td>JavaScript</td>
+              <td>HTML</td>
+              <td>React Native</td>
+              <td>PostgreSQL</td>
+              <td>Docker</td>
+            </tr>
+            <tr>
+              <td>TypeScript</td>
+              <td>CSS</td>
+              <td>Expo</td>
+              <td>MySQL</td>
+              <td>AWS</td>
+            </tr>
+            <tr>
+              <td>Python</td>
+              <td>JavaScript</td>
+              <td>Nativewind</td>
+              <td>SQLite</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Java</td>
+              <td>TypeScript</td>
+              <td></td>
+              <td>MongoDB</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Node.js</td>
+              <td>React</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Nest.js</td>
+              <td>Tailwind</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Express.js</td>
+              <td>Bootstrap</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td className='rounded-bl-xl'>Flask</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td className='rounded-br-xl'></td>
+            </tr>
+          </table>
         </div>
       </section>
 
